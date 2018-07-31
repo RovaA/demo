19dacd494dbae7f67e7b4276d73e6e2c5e6f6023
RovaA <rrakotondrasoa@bocasay.com>
2018-07-31 17:19:10 +0300

    tweak security

--- src/main/java/mg/bocasay/demo/config/SecurityConfig.java
+++ src/main/java/mg/bocasay/demo/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package mg.bocasay.demo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,31 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import mg.bocasay.demo.config.provider.CustomAuthenticationProvider;
-
 @Configuration
 @EnableWebSecurity
 @ComponentScan("mg.bocasay.demo")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	CustomAuthenticationProvider customAuthProvider;
-
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(customAuthProvider);
-		auth.inMemoryAuthentication()
-			.withUser("user")
-			.password(passwordEncoder().encode("user"))
-			.authorities("USER");
+		auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("user")).authorities("USER");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
-		.and()
-			.csrf().disable()
-			.authorizeRequests().antMatchers("/api/personnel/**").authenticated();
+		http.authorizeRequests().anyRequest().authenticated();
 		super.configure(http);
 	}
 
