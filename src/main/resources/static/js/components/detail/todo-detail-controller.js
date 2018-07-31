8738501517f85f0c108597b9298283d85ea7d7d6
RovaA <rrakotondrasoa@bocasay.com>
2018-07-31 15:15:35 +0300

    update system on detail

--- src/main/resources/static/js/components/detail/todo-detail-controller.js
+++ src/main/resources/static/js/components/detail/todo-detail-controller.js
@@ -1,10 +1,23 @@
 Application.Controllers.controller('TodoDetailController', function($scope, $routeParams, todoService) {
 	var todoDetail = this;
 	
+	$scope.editMode = false;
+	
 	let todoId = $routeParams.id; 
 
 	todoService.find(todoId)
 		.then(
 			response => todoDetail.entity = response.data, 
 			response => console.log("error on finding Entity"));
+	
+	todoDetail.changeMode = function() {
+		$scope.editMode = !$scope.editMode;
+	};
+	
+	todoDetail.updateTodo = function() {
+		todoService.createOrUpdate(todoDetail.entity).then(function(response) {
+			todoDetail.entity = response.data;
+			todoDetail.changeMode();
+		});
+	};
   });
