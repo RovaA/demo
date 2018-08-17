a822385ea6917c433019159070e965b083bc897a
RovaA <rrakotondrasoa@bocasay.com>
2018-08-17 16:22:07 +0300

    tweak use constant for url in service

--- src/main/resources/static/js/services/todo-service.js
+++ src/main/resources/static/js/services/todo-service.js
@@ -1,16 +1,16 @@
-Application.Services.factory('todoService', ['$q', '$rootScope', '$http', function($q, $rootScope, $http) {
+Application.Services.factory('todoService', ['$q', '$rootScope', '$http', 'todoUrl', function($q, $rootScope, $http, todoUrl) {
 	return {
 		find : function(id) {
-			return $http.get('/api/todo/' + id);
+			return $http.get(todoUrl + '/' + id);
 		},
 		findAll : function() {
-			return $http.get('/api/todo/list');
+			return $http.get(todoUrl + '/list');
 		},
 		createOrUpdate : function(entity) {
-			return $http.post('/api/todo/create', entity);
+			return $http.post(todoUrl + '/create', entity);
 		},
 		delete : function(entity) {
-			return $http.delete('/api/todo/delete', {params: {id : entity.id}});
+			return $http.delete(todoUrl + '/delete', {params: {id : entity.id}});
 		}
 	};
 }]);
