9c90759ee9c5a0234d73c7a5fc55a316031a31d4
RovaA <rrakotondrasoa@bocasay.com>
2019-03-07 15:58:54 +0300

    fix resource

--- src/main/java/mg/rova/demo/config/WebMvcConfig.java
+++ src/main/java/mg/rova/demo/config/WebMvcConfig.java
@@ -2,9 +2,19 @@ package mg.rova.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/", "/**", "/*/**")
+                .addResourceLocations("/",
+                        "classpath:/META-INF/resources/",
+                        "classpath:/resources/",
+                        "classpath:/static/");
+    }
 }
